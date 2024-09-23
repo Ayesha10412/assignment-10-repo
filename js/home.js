2c6846e610072d5fdf99bd2c68cdc3c4d6fd1d21
Your Name <k.bristi057@gmail.com>
2024-09-23 12:58:11 +0600

    added the banner section

---
+++ js/home.js
@@ -0,0 +1,11 @@
+const donationBtn= document.getElementById('donation-btn');
+const historyBtn= document.getElementById('history-btn');
+
+function btnClicker(event){
+   event.classList.add('bg-primary');
+   event.classList.add('border-none');
+   event.classList.add('text-text');
+   event.classList.add('font-bold');
+
+
+}
